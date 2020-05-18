e19b289733d42e75466fa805b8245b40702ffc4e
Chris O'Brien <chrisobrien2000@outlook.com>
2020-05-18 14:22:31 -0500

    Initial Commit

---
+++ APIKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkplaceManager
+{
+    public static class APIKey
+    {
+        public static string TrelloKey = "388cc0c56e273f81f6f1495cabf8bd0d";
+    }
+}
